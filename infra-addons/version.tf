82d59de724f27627cff87df77438ada306184c3f
Edwin-Oghenetejiri1 <darealthej@gmail.com>
2026-05-21 00:15:45 +0100

    fix: align aws provider version constraints across all modules

--- infra-addons/version.tf
+++ infra-addons/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0, < 7.0"    # wider range to satisfy all modules
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
