7a48c145a960d8fa5010bbd130ca674f6dc5fedc
Edwin-Oghenetejiri1 <darealthej@gmail.com>
2026-05-24 15:49:20 +0100

    fix: pin eks-blueprints-addons to v1.16.3 to fix cycle error

--- infra-addons/main.tf
+++ infra-addons/main.tf
@@ -38,7 +38,7 @@ module "eks" {
 #########################################################################################################
 module "eks_blueprints_addons" {
   source     = "aws-ia/eks-blueprints-addons/aws"
-  version    = "~> 1.0"
+  version    = "1.16.3"
   depends_on = [module.eks]
 
   cluster_name      = module.eks.cluster_name
